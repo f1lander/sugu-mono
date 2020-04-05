eb628b6e0f1def658eed89ea72ea9442fa3fc2e1
Oscar Soriano <oscar.soriano@outlook.com>
2020-04-05 14:52:58 -0600

    Eslint + typescript was kindof implemented, need to start making changes in order to set the configuration correctly

---
+++ packages/sugu-mobile/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    root: true,
+    extends: ['@react-native-community', 'airbnb-typescript', 'prettier', 'prettier/@typescript-eslint', 'prettier/react', 'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'],
+};
