169b066871fcf47de69127857687873c42f4d2a5
Oscar Soriano <oscar.soriano@outlook.com>
2020-04-13 00:02:35 -0600

    Prettier + ESLint configuration (#4)
    * Configured prettier and eslint in sugu-ui, also did monorepo level configs.
    
    * Added eslint plugins and packages for ui and mobile, which mostly share react plugins/configs.
    
    * More config changes and file test in root monorepo husky hooks.

--- packages/sugu-mobile/App.tsx
+++ packages/sugu-mobile/App.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const whiteColor = '#FFF';
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: whiteColor,
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
+
+export default function App(): React.FC {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
