78acc522ec1601234e17362cfacfc02ab8794ee7
Edax Filander Ucles Hernandez <filanderuclez@gmail.com>
2020-04-12 20:48:25 -0500

    adds files for redux, sagas and antd npm

--- packages/sugu-ui/src/App.tsx
+++ packages/sugu-ui/src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Provider } from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import store from './store'
+import saga from './sagas/'
+import Main from './layouts/main'
+import { sagaMiddleware } from './middleware'
 
-export default App;
+const App = () => (
+  <Provider store={store}>
+    <Main />
+  </Provider>
+)
+
+export default App
+
+sagaMiddleware.run(saga)
---
+++ packages/sugu-ui/src/reducers/index.tsx
@@ -0,0 +1,9 @@
+// import navigation from './navigation'
+// import auth from './auth'
+// import something from './something'
+
+// export default {
+//   navigation,
+//   auth,
+//   something
+// }
