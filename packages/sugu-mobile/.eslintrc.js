1a5df82381142b961ddb1fa7826b209df9a8bbce
Oscar Soriano <oscar.soriano@outlook.com>
2020-04-12 21:34:19 -0600

    Added eslint plugins and packages for ui and mobile, which mostly share react plugins/configs.

--- packages/sugu-mobile/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    root: true,
-    extends: ['@react-native-community', 'airbnb-typescript', 'prettier', 'prettier/@typescript-eslint', 'prettier/react', 'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking'],
-};
