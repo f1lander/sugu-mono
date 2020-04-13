c0daa38b11fb36d8f4fb2df71241fec9a1d6822c
Oscar Soriano <oscar.soriano@outlook.com>
2020-04-12 23:59:15 -0600

    More config changes and file test in root monorepo husky hooks.

--- packages/sugu-ui/src/serviceWorker.ts
+++ packages/sugu-ui/src/serviceWorker.ts
@@ -1,4 +1,4 @@
-/* eslint-disable no-console, @typescript-eslint/no-use-before-define */
+/* eslint-disable no-console, @typescript-eslint/no-use-before-define, no-param-reassign */
 
 // This optional code is used to register a service worker.
 // register() is not called by default.
@@ -29,8 +29,9 @@ type Config = {
 
 export function register(config?: Config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    const url = process.env.PUBLIC_URL || '';
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    const publicUrl = new URL(url, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
@@ -39,7 +40,7 @@ export function register(config?: Config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${url}/service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
