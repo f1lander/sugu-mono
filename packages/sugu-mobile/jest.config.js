46bd1e60686abbe973edf822b5678c39acf8e3e3
Oscar Soriano <oscar.soriano@outlook.com>
2020-04-27 19:12:11 -0600

    Added new packages.

--- packages/sugu-mobile/jest.config.js
+++ packages/sugu-mobile/jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|sentry-expo|native-base)',
+  ],
   coverageThreshold: {
     global: {
       branches: 80,
